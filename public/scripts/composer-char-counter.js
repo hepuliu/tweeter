4acf36bc64268db7c40ff68602f5696498e29485
Jessica <hepu24@gmail.com>
2017-11-14 20:58:45 +0000

    add js counter and validate it works

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,9 @@
+//define function to console.log something 
+function validate(){
+  console.log('validated!');
+}
+
+// call function to calidate doc is ready
+$(document).ready(function(){
+  validate();
+});
